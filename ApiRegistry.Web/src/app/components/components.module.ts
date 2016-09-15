492b1f5e76ea99ca23729bfacc9715974d801a87
Quinntyne Brown <quinntynebrown@gmail.com>
2016-09-14 20:40:57 -0400

    update header and footer

--- ApiRegistry.Web/src/app/components/components.module.ts
+++ ApiRegistry.Web/src/app/components/components.module.ts
@@ -3,7 +3,14 @@ import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
 
-const declarables = [];
+import { PageHeaderComponent } from "./page-header.component";
+import { PageFooterComponent } from "./page-footer.component";
+
+const declarables = [
+    PageHeaderComponent,
+    PageFooterComponent
+];
+
 const providers = [];
 
 @NgModule({
